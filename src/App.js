83d08e0d70e388040a576d90befc8f78bb080f6a
meitoc <info@meitoc.com>
2023-06-12 10:38:05 +0700

    rockpaperscissors

--- src/App.js
+++ src/App.js
@@ -12,25 +12,36 @@ function App() {
 }
 
 function Count(){
-  const [hand,handState] = useState(0);
+  const [game,gameState] = useState(0); //0: play, 1: result, 2: stop
+  const [hand,handState] = useState(0); //0: not, 1-3: rock, paper, scissors
   const [hand2nd, hand2ndState] = useState(0);
-  const [game,gameState] = useState(false);
-  // let [hand2nd, hand2ndState] = useState(0);
   const [referee, refereeState] = useState('Hãy chọn "búa", "báo" hoặc "kéo"');
-  function rockPaperScissors(input){
-  handState(input);
-  }
+
   const buttonColor = [{backgroundColor:"green"},{backgroundColor:"green"},{backgroundColor:"green"}];
   if (hand>0) buttonColor[hand-1].backgroundColor = "red";
   let handIcon = game ? choseHandIcon(hand) : "";
   let hand2ndIcon = game ? choseHandIcon(hand2nd) : "";
   let buttonRestart;
-  buttonRestart = !game ? <button onClick = {startGame}>RA TAY</button> : <button onClick = {resetGame}>CHƠI LẠI</button>;
+  buttonRestart = game===0 ? <button onClick = {startGame}>RA TAY</button> : <button onClick = {resetGame}>CHƠI LẠI</button>;
+  if(game === 1) {
+    gameState(2);
+    setTimeout(() => {
+      const compare = hand-hand2nd;
+      const compareShow = compare>0 ? ( compare%2 ? "Bạn thắng" : "Bạn thua") : compare<0 ? ( compare%2 ? "Bạn thua" : "Bạn thắng") : "Hòa nhau rồi!";
+      refereeState(compareShow);
+    }, 600);
+  }
+
+  function rockPaperScissors(input){
+  handState(input);
+  }
+
   function resetGame(){
     handState(0);
-    gameState(false);
+    gameState(0);
     refereeState('Hãy chọn "búa", "bao" hoặc "kéo"');
   }
+  
   function startGame(){
     if(hand!==0) {
       refereeState("Oảnh tù tì");
@@ -41,25 +52,22 @@ function Count(){
         refereeState("Ra cái này");
         let hand2 = Math.floor(Math.random() * 3) + 1;
         hand2ndState(hand2);
-        gameState(true);
-        setTimeout(() => {
-          let compare = hand-hand2;
-          let compareShow = compare>0 ? ( compare%2 ? "Bạn thắng" : "Bạn thua") : compare<0 ? ( compare%2 ? "Bạn thua" : "Bạn thắng") : "Hòa nhau rồi!";
-          refereeState(compareShow);
-        }, 600);
+        gameState(1);
       }, 1200);
     }
   }
+  
   function ChoseButton(props){
     if(props.click) return (<button style = {buttonColor[props.index-1]} onClick = {()=>rockPaperScissors(props.index)}>{choseHandIcon(props.index)}</button>);
     else return (<button style = {buttonColor[props.index-1]}>{choseHandIcon(props.index)}</button>);
   }
+  
   return (
     <div className="container">
       <div clsss="chooseShow">
-        <ChoseButton index={1} click={!game} />
-        <ChoseButton index={2} click={!game} />
-        <ChoseButton index={3} click={!game} />
+        <ChoseButton index={1} click={game===0} />
+        <ChoseButton index={2} click={game===0} />
+        <ChoseButton index={3} click={game===0} />
       </div>
       <div>
         {buttonRestart}
